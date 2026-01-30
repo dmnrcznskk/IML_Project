ea18806d1baf01e4579d445cc74e42bc7e41c6e1
Sekanko <oskar0208@gmail.com>
2026-01-30 03:10:41 +0100

    Stworznie bazy modelu pod keras

---
+++ backend/src/backend/architectures/neural_networks/conv_model.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from typing import Dict, Any, Tuple
+from numpy import ndarray
+from backend.architectures.base_model import BaseModel
+
+from backend.architectures.neural_networks.keras_base import KerasBaseModel
+
+
+class TrafficSignConvNN(KerasBaseModel):
+    """
+    Konwolucyjna sieć neuronowa opartna na modelu bazowym KerasBaseModel
+    """
+
+    def _create_model(
+        self, input_shape: Tuple[int, int], num_classes: int
+    ) -> tf.keras.Model:
+        model = tf.keras.models.Sequential(name="TrafficSignConvNN")
+        k_input_shape = (input_shape[0], input_shape[1], 3)
+
+        # Blok 1
+        model.add(
+            tf.keras.layers.Conv2D(
+                32, (3, 3), padding="same", input_shape=k_input_shape
+            )
+        )
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+
+        # Blok 2
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+
+        # Blok 3
+        model.add(tf.keras.layers.Conv2D(128, (3, 3), padding="same"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+
+        # Głowica klasyfikująca
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(256))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.Dropout(0.5))
+
+        model.add(tf.keras.layers.Dense(num_classes))
+        model.add(tf.keras.layers.Activation("softmax"))
+        model.compile(
+            optimizer="adam",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+        return model
